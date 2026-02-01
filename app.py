221f327df223ec7d5bca29d31f138a3315de99f4
juraganndeso01 <juraganndeso01@gmail.com>
2026-02-01 16:44:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ else:
         
         # Inisialisasi Model dengan path lengkap 'models/' untuk menghindari error 404
         model = genai.GenerativeModel(
-            model_name="models/gemini-1.5-flash", 
+            model_name="models/gemini-2.5-flash", 
             system_instruction=ZENITH_INSTRUCTION
         )
 
